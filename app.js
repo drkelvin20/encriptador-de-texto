7dce21e26d5bfbb24f5c270a9625409f071ebc7b
Kelvin <drkelvin20@gmail.com>
2024-02-23 22:25:38 -0600

    craecion de contantes con reglas de codificado

---
+++ app.js
@@ -0,0 +1,27 @@
+// La letra "e" es convertida para "enter"
+// La letra "i" es convertida para "imes"
+// La letra "a" es convertida para "ai"
+// La letra "o" es convertida para "ober"
+// La letra "u" es convertida para "ufat"
+
+let textoUsuario = '';
+const reglas = {
+    'e': 'enter',
+    'i': 'imes',
+    'a': 'ai',
+    'o': 'ober',
+    'u': 'ufat'
+};
+
+const reglasDesencriptar = {
+    'enter': 'e',
+    'imes': 'i',
+    'ai': 'a',
+    'ober': 'o',
+    'ufat': 'u'
+};
+
+let letra = '';
+let textoEncriptado = '';
+let textoDesencriptado = '';
+let buffer = '';
